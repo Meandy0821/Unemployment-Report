8602a9edecbfaddf58a3fd5d34a3c678d06f8fd8
Meandy0821 <112020245+Meandy0821@users.noreply.github.com>
2022-10-31 19:48:04 -0400

    Update Unemployment.py

--- App/Unemployment.py
+++ App/Unemployment.py
@@ -22,3 +22,22 @@ pprint(parsed_response)
 latest = parsed_response["data"][0]
 print(latest)
 
+
+from statistics import mean
+
+this_year = [d for d in data if "2022-" in d["date"]]
+
+rates_this_year = [float(d["value"]) for d in this_year]
+#print(rates_this_year)
+
+print("-------------------------")
+print("AVG UNEMPLOYMENT THIS YEAR:", f"{mean(rates_this_year)}%")
+print("NO MONTHS:", len(this_year))
+
+from plotly.express import line
+
+dates = [d["date"] for d in data]
+rates = [float(d["value"]) for d in data]
+
+fig = line(x=dates, y=rates, title="United States Unemployment Rate over time", labels= {"x": "Month", "y": "Unemployment Rate"})
+fig.show()
