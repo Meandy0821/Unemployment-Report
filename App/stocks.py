b8d75a70bf4c145c770a92ecca260fbb2fc5b00c
Meandy0821 <112020245+Meandy0821@users.noreply.github.com>
2022-11-08 13:43:25 -0500

    Refactored and tested stocks data fetching

--- App/stocks.py
+++ App/stocks.py
@@ -9,14 +9,23 @@ from app.alpha import API_KEY
 def format_usd(my_price):
     return f"${my_price:,.2f}"
 
+
+def fetch_stocks_data(symbo):
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={API_KEY}&datatype=csv"
+
+    df = read_csv(request_url)
+
+    return df
+
+
 if __name__=="__main__":
 
     print("STOCKS REPORT...")
+    
     symbol = input("Please input a crypto symbol(default")
     print("SYMBOL:", symbol)
 
-    request_url = f"https://www.alphavantage.co/query"
-
+    df = fetch_stocks_data(symbol)
     df = read_csv(request_url)
     print(df.columns)
     print(df.head())
